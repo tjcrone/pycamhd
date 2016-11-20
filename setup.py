e5fc091e8a9c0ff179f562aee1dade4a6983ece0
Timothy Crone <tjcrone@gmail.com>
2016-11-20 14:55:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
-setup(
-    name='camhd',
-    version='0.1',
-    license='MIT',
-    author='Timothy Crone',
-    author_email='tjcrone@gmail.com',
-    url='https://bitbucket.org/tjcrone/camhd',
-    long_description='README.rst',
-    py_modules=['camhd.py'],
-    description='Module for interacting with OOI CamHD video data',
-)
+setup(name='camhd',
+      version='0.1',
+      description='Module for interacting with OOI CamHD video data',
+      long_description='README.rst',
+      url='https://bitbucket.org/tjcrone/camhd',
+      author='Timothy Crone',
+      author_email='tjcrone@gmail.com',
+      license='MIT',
+      packages=['camhd'])
