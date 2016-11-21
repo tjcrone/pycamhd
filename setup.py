5cd8537fe16a013f195f9454c3278d71d20f0daf
Timothy Crone <tjcrone@gmail.com>
2016-11-21 18:42:54 -0500

    Continue development on v0.4dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='camhd',
-      version='0.3',
+      version='0.4dev',
       description='Module for interacting with OOI CamHD video data',
       long_description='README.rst',
       url='https://bitbucket.org/tjcrone/camhd',
