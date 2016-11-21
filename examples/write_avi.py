1c6eba15a1a5fafe40b5074fa35b7626de8f6047
Timothy Crone <tjcrone@gmail.com>
2016-11-20 19:18:53 -0500

    Update examples

--- examples/write_avi.py
+++ examples/write_avi.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # Here we use the camhd module to write a single-frame AVI file, only
 # downloading data from the moov atom and one frame. This AVI file can
-# easily be converted to raw YUV or to PNG using ffmpeg.
+# easily be converted to a PNG or losslessly to a raw YUV using ffmpeg.
 #
 # Timothy Crone (tjcrone@gmail.com)
 
 import camhd
 
-# file to work with
+# remote file
 filename = 'https://rawdata.oceanobservatories.org/files/RS03ASHS/PN03B/06-CAMHDA301/2016/11/13/CAMHDA301-20161113T000000Z.mov'
 
 # write single frame to avi file
